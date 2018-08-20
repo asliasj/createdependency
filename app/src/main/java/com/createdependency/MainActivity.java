65f1469ff4f27b7da7d17aa14d58ffd3b07cbd74
asliasj <42487683+asliasj@users.noreply.github.com>
2018-08-20 12:49:41 +0530

    finally not
    finally not  rgya yr

--- app/src/main/java/com/createdependency/MainActivity.java
+++ app/src/main/java/com/createdependency/MainActivity.java
@@ -1,9 +1,11 @@
 package com.createdependency;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.e.myutils.Main2Activity;
 import com.e.myutils.mymath;
 
 
@@ -17,5 +19,7 @@ public class MainActivity extends AppCompatActivity {
         int a=mymath.plus(20,30);
         Toast.makeText(this, ""+a, Toast.LENGTH_SHORT).show();
 
+        Intent i = new Intent(this , Main2Activity.class);
+        startActivity(i);
     }
 }
